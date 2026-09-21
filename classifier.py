ef4121a3cf556dd37cbe5795a5383de1f5d47fb1
skishore-dhcp <skish2024@gmail.com>
2026-09-21 11:00:56 +0530

    Update classifier.py

--- classifier.py
+++ classifier.py
@@ -1,10 +1,3 @@
-"""
-classifier.py
-
-A small gatekeeper: checks whether the user's input is actually a
-purchase decision question before running the full three-persona debate.
-"""
-
 from llm_client import get_response
 
 CLASSIFIER_INSTRUCTION = """You are a strict classifier. You will be given a single sentence or question. Decide whether it describes a purchase decision the user is considering making (e.g. buying an item, subscribing to a service, spending money on something).
